2e92d832230da29d80cdd07de330934133574111
ruraeru <f93335383@gmail.com>
2025-09-22 10:56:23 +0900

    feat: 만약에 토큰이 만료되어 요청 시 401 에러가 발생한다면 토큰을 새로 발급 받아서 다시 요청을 보내도록 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -5,6 +5,13 @@ import type {
 } from 'axios';
 import axios from 'axios';
 import { useSpotifyStore } from '../store/spotify';
+import { authSpotify } from './spotify/spotify';
+
+declare module 'axios' {
+  export interface InternalAxiosRequestConfig {
+    _retry?: boolean;
+  }
+}
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SPOTIFY_BASE_URL,
@@ -25,6 +32,38 @@ axiosInstance.interceptors.request.use(
     return config;
   },
   (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error: AxiosError) => {
+    const originalRequest = error.config as InternalAxiosRequestConfig;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        console.log('액세스 토큰 만료. 토큰을 갱신합니다...');
+        const tokenData = await authSpotify();
+        const { setSpotify } = useSpotifyStore.getState();
+        setSpotify(tokenData);
+
+        originalRequest.headers.Authorization = `${tokenData.token_type} ${tokenData.access_token}`;
+
+        console.log('새 토큰으로 원래 요청을 재시도합니다.');
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        console.error('토큰 갱신에 실패했습니다:', refreshError);
+        const { clear } = useSpotifyStore.getState();
+        clear();
+
+        return Promise.reject(refreshError);
+      }
+    }
     return Promise.reject(error);
   },
 );
